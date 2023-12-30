e71789e61b5eeb5f0aca75b152b95066ed54b502
akippix <akippix@gmail.com>
2023-12-31 07:16:50 +0900

    Added setting script

---
+++ setting.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Vim Packer
+if [ -e ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
+  rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+CURRENT=$(pwd)
+
+if [ -e ~/.config/nvim/init.lua ]; then
+  unlink ~/.config/nvim/init.lua
+fi
+mkdir -p ~/.config/nvim
+ln -s $CURRENT/nvim/init.lua ~/.config/nvim
+
+if [ -e ~/.zshrc ]; then
+  unlink ~/.zshrc
+fi
+ln -s $CURRENT/.zshrc ~
+
+if [ -e ~/.gitignore ]; then
+  unlink ~/.gitignore
+fi
+ln -s $CURRENT/.gitignore ~
